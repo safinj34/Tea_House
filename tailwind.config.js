311f7d113b2e6fa5ec09d01ce50324f0fe47c8f8
safinj34 <safinj34@gmail.com>
2026-01-28 07:23:52 +0600

    small medium responsive

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx,html}"],
+  theme: {
+    screens: {
+      xs: "480px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+    },
+  },
+};
